626a47042e126d55e7c61f4f1623efe2374ee9a7
jslatteryCCC <s00441786@acad.tri-c.edu>
2022-10-01 11:18:52 -0400

    Week 3/Lab 3 Commit

---
+++ htdocs/Week3/courses.php
@@ -0,0 +1,17 @@
+<?php 
+
+$dsn = 'mysql:host=localhost;dbname=it1150';
+$username = 'root';
+$password = '';
+
+try{
+    $db = new PDO($dsn, $username, $password);
+    echo '<div class="db_msg_div"><p class="db_msg">Connection to database successful</p></div>';
+}catch (PDOException $e){
+    $error_message = $e->getMessage();
+    include('db_error.php');
+    exit();
+}
+
+
+?>
---
+++ htdocs/Week3/db_error.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html> 
+
+<html>
+    <head>
+        <title>IT Courses</title> 
+        <link rel="stylesheet" type="text/css" href="main.css"> </head>
+    <body> 
+        <main>
+            <h1>Database Error</h1>
+            <p>There was an error connecting to the database.</p> 
+            <p>Error message: <?php echo $error_message; ?></p>
+        </main> 
+    </body>
+</html> 
